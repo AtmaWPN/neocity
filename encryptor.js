ca23c2e55104d8e726c6690a4c85c5e2fda9beb5
Albin Dittli <ajdittli@gmail.com>
2025-05-22 16:47:04 -0600

    added demo code

--- encryptor.js
+++ encryptor.js
@@ -5,13 +5,25 @@ const inputContent = document.getElementById("blog-content-input");
 function encrypt() {
   outputContent.innerHTML = inputContent.value;
   if (!key.value || !inputContent.value) return;
-  
+
+  const regex = /\<[^\>]\>/g;
+  const tags = regex.exec(inputContent.value);
+  // demo code from stackoverflow
+  var re = /bar/g,
+  str = "foobarfoobar";
+  while ((match = re.exec(str)) != null) {
+      console.log("match found at " + match.index);
+  }
+
   let charCodes = [];
   for (let i = 0; i < inputContent.value.length; i++) {
+    // if character is in a tag, skip
+    if (tags)
+
     charCodes.push(inputContent.value.charCodeAt(i) - key.value.charCodeAt(i % key.value.length));
   }
   const cipherText = String.fromCharCode(...charCodes);
   console.log(cipherText);
-  outputContent.innerHTML = btoa(cipherText);
+  outputContent.textContent = btoa(cipherText);
   console.log(btoa(cipherText));
 }
